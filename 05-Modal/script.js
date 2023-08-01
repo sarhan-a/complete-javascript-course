9211ee2e0d6bdd6301d3fc97611e226439add4d8
Sarhan Alim <sarhan.alim@gmail.com>
2023-08-01 22:58:50 +0100

    adding / removing classes & handling keypress events

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+console.log(btnShowModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (ev) {
+  ev.key === 'Escape' && !modal.classList.contains('hidden') ? closeModal() : '';
+})
