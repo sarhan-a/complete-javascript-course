b97931044fa0127d9f4bd27c6638bda8ce557095
Sarhan Alim <sarhan.alim@gmail.com>
2023-08-07 22:00:32 +0100

    switch the active player and reset score to 0 if 1 is rolled on dice

--- 06-Pig-Game/script.js
+++ 06-Pig-Game/script.js
@@ -1,9 +1,12 @@
 'use strict';
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +34,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore // Change later
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-    // switch to next player
+    // switch to next player and reset current score to 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
